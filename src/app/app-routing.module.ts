1631128127098fad4796a695a5dc932038a815c2
Daniel Persson <persson.daniel.1990@gmail.com>
2021-02-14 11:17:45 +0100

    Begin working with routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { 
+    path: '',
+    component: HomeComponent,
+    children: [
+    {
+      path: 'home',
+      component: HomeComponent,
+      redirectTo: ''
+    },
+    {
+      path: 'index',
+      component: HomeComponent,
+      redirectTo: ''
+    }
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
